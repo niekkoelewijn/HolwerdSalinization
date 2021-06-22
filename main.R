a2eba43d3baa866ef120e27bcb020beaa956aa36
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2021-06-22 09:16:23 +0200

    added comments

--- main.R
+++ main.R
@@ -5,16 +5,30 @@
 # Date: june 2021
 # License: GNU General Public License version 3.0
 
-library(raster)
+# Install required packages
+if(!"raster" %in% rownames(installed.packages())){install.packages("raster")}
+if(!"rgdal" %in% rownames(installed.packages())){install.packages("rgdal")}
+if(!"sf" %in% rownames(installed.packages())){install.packages("sf")}
+if(!"shiny" %in% rownames(installed.packages())){install.packages("shiny")}
+if(!"shinythemes" %in% rownames(installed.packages())){install.packages("shinythemes")}
+if(!"leaflet" %in% rownames(installed.packages())){install.packages("leaflet")}
 
+library(shiny)
+library(raster)
+library(shinythemes)
+library(leaflet)
+library(sf)
+
+# Make a data directory
 datdir <- "./data"
 if (!dir.exists(datdir)){
   dir.create(datdir, showWarnings = FALSE)
 }
 
+# Download MODIS data from drive
 downloadlink <- "https://drive.google.com/uc?export=download&id=1gAUnzkTQxME1eWIgWvgMVmtOajlGRAUW"
-
 download.file(url = downloadlink, destfile = './data/April2020.tif', method = 'auto')
 
+# Load the Sentinel data into a raster brick
 April2020 <- brick("./data/April2020.tif")
 
