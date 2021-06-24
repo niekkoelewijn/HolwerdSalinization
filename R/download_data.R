c7f20f264dcfd2586f5463a08d0aa930930dac04
thijskoop <thijskoop@gmail.com>
2021-06-24 09:31:24 +0200

    adjusted download data for datta on github

--- R/download_data.R
+++ R/download_data.R
@@ -8,7 +8,7 @@
 download_data = function(url, savename) {
   #Function that download file from url and saves it. Returns a RasterBrick
   #Two arguments. url and savename how it should be saved in the data folder
-  destinationname <- paste0('data/',savename,'.tif')
+  destinationname <- paste0('./data/',savename,'.tif')
   download.file(url = as.character(url), destfile = destinationname, method = 'auto') 
   Rasterbrick = brick(destinationname)
   return(Rasterbrick)
