b40d3adb578ad205636f0c95b96f50a3baae13b4
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2021-06-24 21:09:20 +0200

    added a script for the graph of the means

---
+++ R/graph_visualization.R
@@ -0,0 +1,14 @@
+# Project Holwerd Salinization
+# RS and GIS integration
+# Wageningen University
+# Group 5
+# Date: june 2021
+# License: GNU General Public License version 3.0
+
+# Create a dates variable in the correct format
+dates <- c("2019-03-19", "2019-03-29", "2019-04-16", "2019-04-23", "2019-05-13",
+           "2019-05-16", "2019-06-22", "2019-07-17", "2019-07-27", "2019-08-24",
+           "2019-08-26", "2019-09-10", "2019-09-13", "2019-10-10", "2019-10-30",
+           "2019-12-12")
+
+# Create graph to visualize corn NDRE development over time
