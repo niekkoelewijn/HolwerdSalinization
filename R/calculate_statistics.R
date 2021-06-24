63ca4be159487a5bd85f554a7f29ad66535c6175
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2021-06-24 11:25:27 +0200

    now able to create columns for all dates, but values do not change to mean values but stay NA values...

--- R/calculate_statistics.R
+++ R/calculate_statistics.R
@@ -6,20 +6,33 @@
 # License: GNU General Public License version 3.0
 
 # Create a function to calculate the mean VI value for a plot per given date 
-calculate_statistics <- function(plots, date, VI){
+calculate_statistics <- function(plots, name, number, VI){
   if(VI == "NDRE"){
-    plots$date <- exact_extract(s2_images_NDRE[[date]], plots, 'mean')
+    plots[name] <- exact_extract(s2_images_NDRE[[number]], plots, 'mean')
   }
   if(VI == "NDVI"){
-    plots$date <- exact_extract(s2_images_NDVI[[date]], plots, 'mean')
+    plots[name] <- exact_extract(s2_images_NDVI[[number]], plots, 'mean')
   }
 }
 
-# Create columns for Corn_plots
+# Create columns for the mean values of the VI's per date for both crops
 for (i in s2_images_names){
-  Corn_plots[i] <- NA
+  Corn_plots_NDVI[i] <- NA
 }
 
+for (i in s2_images_names){
+  Corn_plots_NDRE[i] <- NA
+}
+
+for (i in s2_images_names){
+  Potato_plots_NDVI[i] <- NA
+}
+
+for (i in s2_images_names){
+  Potato_plots_NDRE[i] <- NA
+}
+
+# Calculate the mean 
 for (i in 1:length(s2_images_names)){
-  calculate_statistics(Corn_plots, as.factor(s2_images_names[i]), "NDVI")
+  calculate_statistics(plots=Corn_plots_NDVI, name=s2_images_names[i], number=i, VI="NDVI")
 }
