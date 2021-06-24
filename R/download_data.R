40fe191ba19e9645f2f7340e36819862d89e2faa
thijskoop <thijskoop@gmail.com>
2021-06-24 09:51:20 +0200

    adjusted download data for github

--- R/download_data.R
+++ R/download_data.R
@@ -9,7 +9,9 @@ download_data = function(url, savename) {
   #Function that download file from url and saves it. Returns a RasterBrick
   #Two arguments. url and savename how it should be saved in the data folder
   destinationname <- paste0('./data/',savename,'.tif')
-  download.file(url = as.character(url), destfile = destinationname, method = 'auto') 
+  #The line below downloads data from a dropbox. This gives problems when run on different operating systems 
+  #Windows and OS, therefore we now have the data on github
+  #download.file(url = as.character(url), destfile = destinationname, method = 'auto') 
   Rasterbrick = brick(destinationname)
   return(Rasterbrick)
 } 
