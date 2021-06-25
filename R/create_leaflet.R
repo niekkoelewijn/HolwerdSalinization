d41227205e87d09749c58c0409d338a3a57cd559
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2021-06-25 14:04:15 +0200

    progression on leaflet

--- R/create_leaflet.R
+++ R/create_leaflet.R
@@ -24,15 +24,21 @@ potato_val_NDRE <- Potato_plots_NDRE[5:8,(1+3)]
 #create leaflet of NDVI
 #pal <-  colorBin(palette = "RdYlGn", domain = corn_mon_NDVI)
 
-leaflet(corn_mon_NDVI) %>%
+leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   addRasterImage(x=s2_images_NDVI[[1]], colors = "RdYlGn", opacity = 0.8, group="NDVI raster") %>%
   #addLegend("bottomright", pal=pal, values= s2_images_NDVI[[1]]@data@values, title = "NDVItitle", opacity= 1) %>%
-  addLayersControl( 
-    overlayGroups = c("NDVI raster", "Corn_mon", "Corn_val", "Potato_mon", "Potato_val"),
-    options = layersControlOptions(collapsed = FALSE)) %>%
+  #addLayersControl( 
+    #overlayGroups = c("NDVI raster", "Corn_mon", "Corn_val", "Potato_mon", "Potato_val"),
+    #options = layersControlOptions(collapsed = FALSE)) %>%
   addScaleBar(position = "bottomleft") %>%
-  addPolygons(data = corn_mon_NDVI, color="Black")
+  addPolygons(data = st_zm(corn_mon_NDVI), color="Black", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5, fillColor = ~colorQuantile("YlOrRd", corn_mon_NDVI)(corn_mon_NDVI))
 
+leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = corn_mon_NDVI, color="Black", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5)
 
+  
 
